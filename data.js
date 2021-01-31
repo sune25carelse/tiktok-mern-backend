001203201abb8039ccbeb164518fc4454b301e11
Sune Carelse <sune.carelse@takealot.com>
2021-01-31 21:20:23 +0200

    make it better

---
+++ data.js
@@ -0,0 +1,23 @@
+const data = [
+    {
+        url: "https://static.videezy.com/system/resources/previews/000/005/575/original/matrix.mp4",
+        channel: "luca",
+        song: "kickin it late yeah",
+        likes: "1223",
+        messages: "456",
+        description: "it f**kn works",
+        shares: "789",
+    },
+
+    {
+        url: "https://static.videezy.com/system/resources/previews/000/005/575/original/matrix.mp4",
+        channel: "luca",
+        song: "kickin it late yeah",
+        likes: "1223",
+        messages: "456",
+        description: "it f**kn works",
+        shares: "789",
+    },
+];
+
+export default data;
